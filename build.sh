cc1a765115655bc5cadfdcf3536e0eb11bf7a57a
JonghyeokPark <akindo19@skku.edu>
2022-05-11 17:46:36 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -29,15 +29,15 @@ elif [ "$1" = "--nc-monitor" ]; then
     # Cache hot LB pages with mtr-logging/monitoring enabled
     BUILD_FLAGS="-DUNIV_NVDIMM_CACHE -DUNIV_LOG_HEADER -DUNIV_FLUSH_MONITOR"
 else
-    # Cache hot LB pages (default)
-    BUILD_FLAGS="-DUNIV_NVDIMM_CACHE -DUNIV_LOG_HEADER"
+    # Cache NVDIMM pages in TPC-C workloads
+    BUILD_FLAGS="-DUNIV_NVDIMM_CACHE"
 fi
 
 echo "Start build using $BUILD_FLAGS"
 
-cd $BASE_DIR
+cd $BUILD_DIR
 
-cmake -DWITH_DEBUG=0 -DCMAKE_C_FLAGS="$BUILD_FLAGS" -DCMAKE_CXX_FLAGS="$BUILD_FLAGS" \
+cmake .. -DWITH_DEBUG=0 -DCMAKE_C_FLAGS="$BUILD_FLAGS" -DCMAKE_CXX_FLAGS="$BUILD_FLAGS" \
 -DDOWNLOAD_BOOST=ON -DWITH_BOOST=$BASE_DIR/boost -DENABLED_LOCAL_INFILE=1 \
 -DCMAKE_INSTALL_PREFIX=$BUILD_DIR
 
